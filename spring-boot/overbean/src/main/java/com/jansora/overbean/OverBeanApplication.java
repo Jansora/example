d327fc540f8c39cf718ef7f1121b014f37ef20c6
Jansora <zhangyue1936@gmail.com>
2021-10-10 17:52:04 +0800

    init overbean

--- spring-boot/overbean/src/main/java/com/jansora/overbean/OverBeanApplication.java
+++ spring-boot/overbean/src/main/java/com/jansora/overbean/OverBeanApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OverbeanApplication {
+public class OverBeanApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OverbeanApplication.class, args);
+        SpringApplication.run(OverBeanApplication.class, args);
     }
 
 }
--- spring-boot/overbean/src/test/java/com/jansora/overbean/OverBeanApplicationTests.java
+++ spring-boot/overbean/src/test/java/com/jansora/overbean/OverBeanApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OverbeanApplicationTests {
+class OverBeanApplicationTests {
 
     @Test
     void contextLoads() {
